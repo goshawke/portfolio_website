e971b3eac860761c683cb1e723ed1d80c0ca1811
goshawke <goshawke@hushmail.com>
2024-06-13 17:43:26 -0400

    add color changing mouse hover events to contact buttons

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,7 @@
+
+/* Adds functionality to the profile image. When hovered over, it replaces the B&W image with the Color image.
+When the mouse leaves, the image reverts
+*/
 const profile_img = document.getElementById("profile-img");
 
 profile_img.addEventListener('mouseover', (event) => {
@@ -6,4 +10,36 @@ profile_img.addEventListener('mouseover', (event) => {
 
 profile_img.addEventListener('mouseout', (event) =>  {
     event.target.src="images/portrait.jpg";
+});
+
+
+/* Adds functionality to the contact icons. When each is hovered over, its color will change.
+When the mouse leaves, the color reverts
+*/
+
+const email_icon = document.getElementById("email-icon");
+
+email_icon.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+});
+email_icon.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'white';
+});
+
+const linkedin_icon = document.getElementById("linkedin-icon");
+
+linkedin_icon.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+});
+linkedin_icon.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'white';
+});
+
+const github_icon = document.getElementById("github-icon");
+
+github_icon.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'blue';
+});
+github_icon.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'white';
 });
