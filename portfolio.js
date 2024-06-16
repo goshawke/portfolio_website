10f1086104986127f769568b98edb6d0044e11d9
goshawke <goshawke@hushmail.com>
2024-06-16 15:58:45 -0400

    adds new header and text font

--- portfolio.js
+++ portfolio.js
@@ -57,7 +57,7 @@ if(github_icon){
 
 
 /*
-Adds functionality to the header buttons. When each is hovered over, its color will change.
+Adds functionality to the header buttons. When each is hovered over, its color and size will change.
 When the mouse leaves, the color reverts
 */
 
@@ -65,9 +65,11 @@ const home_btn = document.getElementById("home-btn");
 if(home_btn){
     home_btn.addEventListener('mouseover', (event) => {
         event.target.style.color = '#1ABC9C';
+        event.target.style.fontSize="2.25rem";
     });
     home_btn.addEventListener('mouseout', (event) => {
         event.target.style.color = '#BDC3C7';
+        event.target.style.fontSize="2rem";
     });
 }
 
@@ -76,11 +78,14 @@ const projects_btn = document.getElementById("projects-btn");
 if(projects_btn){
     projects_btn.addEventListener('mouseover', (event) => {
         event.target.style.color = '#1ABC9C';
+        event.target.style.fontSize="2.25rem";
     });
     projects_btn.addEventListener('mouseout', (event) => {
         event.target.style.color = '#BDC3C7';
+        event.target.style.fontSize="2rem";
     });
 }
+
 
 
 /*
@@ -88,12 +93,45 @@ Adds functionality to the project link. When each is hovered over, its color wil
 When the mouse leaves, the color reverts
 */
 
-const proj_link = document.getElementById("project-link");
-if(proj_link){
-    proj_link.addEventListener('mouseover', (event) => {
+const proj_link1 = document.getElementById("project-link1");
+if(proj_link1){
+    proj_link1.addEventListener('mouseover', (event) => {
         event.target.style.color = '#1ABC9C';
     });
-    proj_link.addEventListener('mouseout', (event) => {
+    proj_link1.addEventListener('mouseout', (event) => {
+        event.target.style.color = '#BDC3C7';
+    });
+}
+
+
+const proj_link2 = document.getElementById("project-link2");
+if(proj_link2){
+    proj_link2.addEventListener('mouseover', (event) => {
+        event.target.style.color = '#1ABC9C';
+    });
+    proj_link2.addEventListener('mouseout', (event) => {
+        event.target.style.color = '#BDC3C7';
+    });
+}
+
+
+const proj_link3 = document.getElementById("project-link3");
+if(proj_link3){
+    proj_link3.addEventListener('mouseover', (event) => {
+        event.target.style.color = '#1ABC9C';
+    });
+    proj_link3.addEventListener('mouseout', (event) => {
+        event.target.style.color = '#BDC3C7';
+    });
+}
+
+
+const proj_link4 = document.getElementById("project-link4");
+if(proj_link4){
+    proj_link4.addEventListener('mouseover', (event) => {
+        event.target.style.color = '#1ABC9C';
+    });
+    proj_link4.addEventListener('mouseout', (event) => {
         event.target.style.color = '#BDC3C7';
     });
 }
